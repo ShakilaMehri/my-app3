6740427b1b6be70f00cf640fcbd219434df53a54
Shakila <shakila.mehri.98@gmail.com>
2024-08-14 12:22:22 +0330

    added feature

---
+++ src/app/components/feature.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../styles/feature.module.css";
+
+const Feature = () => {
+  return (
+    <section className={styles.feature}>
+      <div className={styles.featureSection}>
+      <h2>Find the</h2>
+      <span>perfect Gifts</span>
+      <p>To celebrate a special moment</p>
+      </div>
+      <div className={styles.featureImg}>
+      <img src="../../../images/bascket.jfif" alt="gifts" />
+      </div>
+    </section>
+  );
+};
+
+export default Feature;
