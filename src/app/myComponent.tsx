6a3966ebeb4343fe4400f82c5ea82008a6d492b5
Shakila <shakila.mehri.98@gmail.com>
2024-08-21 22:03:46 +0330

    Updated project with new changes

--- src/app/myComponent.tsx
+++ src/app/myComponent.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Shop from "./shop/shop";
 import "./global.css";
 
 const queryClient = new QueryClient();
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-    const root = createRoot(rootElement);
-
-
- root.render(
-    <QueryClientProvider client={queryClient}>
-        <React.StrictMode>
-        <Shop/>
-        </React.StrictMode>
-    </QueryClientProvider>
-);
-} else {
-    console.error("Root element not found");
+export default function App () {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Shop/>
+        </QueryClientProvider>
+    )
 }
