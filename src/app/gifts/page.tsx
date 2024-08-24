8b38adb9ab385c588305f2a8337806593fffb074
Shakila <shakila.mehri.98@gmail.com>
2024-08-24 17:18:47 +0330

    added page

---
+++ src/app/gifts/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/header';
+import Slider from './slider';
+import Footer from '../components/footer';
+
+const Page = () => {
+  return (
+    <div>
+        <Header/>
+        <Slider/>
+        <Footer/>
+    </div>
+  );
+};
+
+export default Page;
