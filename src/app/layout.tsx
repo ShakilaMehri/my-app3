9ee6206787839139a4a63e634338a6f51afec78b
Shakila <shakila.mehri.98@gmail.com>
2024-08-24 17:19:18 +0330

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const vazirFont = localFont({src: "../../public/font/Vazirmatn-Regular.woff2" });
 
 export const metadata: Metadata = {
   title: "olive&avery",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={vazirFont.className}>{children}</body>
     </html>
   );
 }
