7129eaa0b70eea14b4151e57fe20ee65b2a11dd6
Shakila <shakila.mehri.98@gmail.com>
2024-08-29 20:04:39 +0330

    updated server

--- server.js
+++ server.js
@@ -1,50 +1,22 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const cors = require('cors');
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
 
-app.get('/items', (req, res) => {
-  const filePath = path.join(__dirname, 'items.json');
-  
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('File read error:', err);
-      return res.status(500).json({ message: 'Error reading items.json file' });
-    }
-    
-    try {
-      const jsonData = JSON.parse(data);
-      res.json(jsonData);
-    } catch (jsonErr) {
-      console.error('JSON parse error:', jsonErr);
-      res.status(500).json({ message: 'Error parsing JSON', error: jsonErr.message });
-    }
-  });
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my express server! Use /items to get the list of items");
 });
 
-app.get('/items/:id', (req, res) => {
-  const filePath = path.join(__dirname, 'items.json');
-  
-  fs.readFile(filePath, 'utf8', (err, data) => {
+app.get("/items", (req, res) => {
+  fs.readFile("items.json", "utf-8", (err, data) => {
     if (err) {
-      console.error('File read error:', err);
-      return res.status(500).json({ message: 'Error reading items.json file' });
-    }
-    
-    try {
-      const jsonData = JSON.parse(data);
-      const item = jsonData.items.find(i => i.id === parseInt(req.params.id, 10));
-      
-      if (item) {
-        res.json(item);
-      } else {
-        res.status(404).json({ message: 'Item not found' });
-      }
-    } catch (jsonErr) {
-      console.error('JSON parse error:', jsonErr);
-      res.status(500).json({ message: 'Error parsing JSON', error: jsonErr.message });
+      res.status(500).send("Error reading file");
+    } else {
+      res.json(JSON.parse(data));
     }
   });
 });
