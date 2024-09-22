bd0ba506d5d44256dd505b431d39c897c159c19a
Shakila <shakila.mehri.98@gmail.com>
2024-09-22 13:23:29 +0330

    upgraded cartContext

---
+++ src/app/context/cartContext.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+import { CartItemType } from "../shop/shop";
+
+interface CartContextType {
+  cartItems: CartItemType[];
+  addToCart: (item: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [cartItems, setCartItems] = useState<CartItemType[]>(() => {
+    if (typeof window !== "undefined") {
+      const storedCartItems = localStorage.getItem("cart");
+      try {
+        return storedCartItems ? JSON.parse(storedCartItems) : [];
+      } catch (error) {
+        console.error("Error parsing stored cart items:", error);
+        return[];
+      }
+    }
+    return[];
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedCartItems = localStorage.getItem("cart");
+      console.log("Loaded cart items from localStorage:", storedCartItems); // Check here
+      setCartItems(storedCartItems ? JSON.parse(storedCartItems) : []);
+    }
+  }, []);
+
+  const addToCart = (item: CartItemType) => {
+    setCartItems((prev) => {
+      const isItemInCart = prev.find((cartItem) => cartItem.id === item.id);
+      if (isItemInCart) {
+        return prev.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, amount: cartItem.amount + 1 }
+            : cartItem
+        );
+      }
+      return [...prev, { ...item, amount: 1 }];
+    });
+  };
+
+  const removeFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((acc, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return acc;
+          return [...acc, { ...item, amount: item.amount - 1 }];
+        }
+        return [...acc, item];
+      }, [] as CartItemType[])
+    );
+  };
+
+  if (!cartItems) return null;
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCart = (): CartContextType => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
+
+export { CartProvider, useCart };
