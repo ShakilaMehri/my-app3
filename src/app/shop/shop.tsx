e6a3c65a818c35702c1b2a0384196180afac8afc
Shakila <shakila.mehri.98@gmail.com>
2024-09-28 17:11:28 +0330

    upgraded shop

--- src/app/shop/shop.tsx
+++ src/app/shop/shop.tsx
@@ -8,6 +8,7 @@ import Item from "../item/item";
 import { Wrapper} from "./shop.styles";
 import SideBar from "../components/sidebar";
 import { GlobalStyle } from "../sidebar/sidebar";
+import { useCart } from "../context/cartContext";
 
 export type CartItemType = {
   id: number;
@@ -16,11 +17,6 @@ export type CartItemType = {
   imgUrl?: string;
   amount: number;
 };
-
-interface CartProps {
-  cartItems: CartItemType[];
-  setCartItems: React.Dispatch<React.SetStateAction<CartItemType[]>>;
-}
 
 const fetchItems = async (): Promise<CartItemType[]> => {
   try {
@@ -40,43 +36,12 @@ const fetchItems = async (): Promise<CartItemType[]> => {
 };
 
 const Shop = () => {
-  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+  const {cartItems, addToCart, removeFromCart} = useCart();
   const { isLoading, error, data: itemsData } = useQuery<CartItemType[], Error>({
     queryKey: ["items"],
     queryFn: fetchItems,
     initialData: [],
   });
-
-  useEffect(() => {
-    console.log("Cart Items have been updated:", cartItems);
-  }, [cartItems]);
-
-  const handleAddToCart = (clickedItem: CartItemType) => {
-    setCartItems((prev) => {
-      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
-      if (isItemInCart) {      
-        return prev.map((item) =>
-          item.id === clickedItem.id
-            ? { ...item, amount: item.amount + 1 }
-            : item
-        );
-      }
-      return [...prev, { ...clickedItem, amount: 1 }];
-    });
-  };
-
-  const handleRemoveFromCart = (id: number) => {
-    setCartItems((prev) =>
-      prev.reduce((acc, item) => {
-        if (item.id === id) {
-          if (item.amount === 1) return acc;
-          return [...acc, { ...item, amount: item.amount - 1 }];
-        } else {
-          return [...acc, item];
-        }
-      }, [] as CartItemType[])
-    );
-  };
   
   if (isLoading) return <Spinner />;
   if (error) return <div>An error has occurred: {error.message}</div>;
@@ -93,8 +58,8 @@ const Shop = () => {
                 key={item.id}
                 item={item}
                 cartItems={cartItems}
-                handleAddToCart={handleAddToCart}
-                handleRemoveFromCart={handleRemoveFromCart}
+                handleAddToCart={addToCart}
+                handleRemoveFromCart={removeFromCart}
               />
             ))}
       </Grid>
