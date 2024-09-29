b63481b92fd52ae01a525de773e4a0149779f49d
Shakila <shakila.mehri.98@gmail.com>
2024-09-29 17:26:54 +0330

    updated page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Cart from "./cart";
-import SideBar from "../components/sidebar";
-import { GlobalStyle } from "../sidebar/sidebar";
 import { CartProvider} from "../context/cartContext";
 
 const Page = () => {
   return (
     <CartProvider>
-      {/* <SideBar />
-      <GlobalStyle /> */}
       <Cart/>
     </CartProvider>
   );
