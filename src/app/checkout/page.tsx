6581a97a731f2d3ba00395d591c71b263e4a732e
Shakila <shakila.mehri.98@gmail.com>
2024-09-29 14:21:40 +0330

    updated checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,19 +2,40 @@
 
 import React from 'react';
 import { useCart } from '../context/cartContext';
+import styles from "../styles/checkout.module.css";
 
 const CheckOut: React.FC = () => {
   const {cartItems} = useCart();
+
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.amount * item.price,
+    0
+  );
   return (
-    <div>
-        <h1>CheckOut</h1>
-        <p>Your order has been placed!</p>
-        <h2>Order Summary:</h2>
-        {cartItems.map(item => (
-          <div key={item.id}>
-            <p>{item.name} x {item.amount}</p>
+    <div className={styles.checkoutContainer}>
+        <h1 className={styles.checkoutHeader}>CheckOut</h1>
+        {cartItems.length > 0 ? (
+          <>
+          <p>Your order has been placed!</p>
+          <div className={styles.orderSummary}>
+            <h2>Order Summary:</h2>
+            {cartItems.map((item) => (
+              <div key={item.id} className={styles.orderItem}>
+                <img src={item.imgUrl} alt={item.name} />
+                <div className={styles.itemDetails}>
+                <p>{item.name} x {item.amount}</p>
+                </div>
+                <p>${(item.amount * item.price).toFixed(2)}</p>
+              </div>
+            ))}
           </div>
-        ))}
+          <div className={styles.totalPrice}>
+            Total: ${totalPrice.toFixed(2)}
+          </div>
+          </>
+        ) : (
+          <p className={styles.emptyCartMessage}>Your cart is empty!</p>
+        )}
     </div>
   );
 };
